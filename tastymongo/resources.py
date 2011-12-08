78958aebd38a73e08ef23ad84d4e55e96a2ffda8
Niels Sandholt Busch <niels.busch@gmail.com>
2011-12-08 11:12:56 +0100

    fix obj_get

--- tastymongo/resources.py
+++ tastymongo/resources.py
@@ -63,11 +63,25 @@ class ModelResource(Resource):
         return self._meta.object_class.objects.all()
 
     def obj_get(self, request=None, **kwargs):
+        """
+        A ORM-specific implementation of ``obj_get``.
+
+        Takes optional ``kwargs``, which are used to narrow the query to find
+        the instance.
+        """
         try:
-            obj = self._meta.object_class.objects.get(pk=kwargs.get('pk'))
-        except self._meta.object_class.DoesNotExist:
-            raise NotFound("The resource could not be found")
-        return obj
+            base_object_list = self.get_object_list(request).filter(**kwargs)
+            object_list = self.apply_authorization_limits(request, base_object_list)
+            stringified_kwargs = ', '.join(["%s=%s" % (k, v) for k, v in kwargs.items()])
+
+            if len(object_list) <= 0:
+                raise self._meta.object_class.DoesNotExist("Couldn't find an instance of '%s' which matched '%s'." % (self._meta.object_class.__name__, stringified_kwargs))
+            elif len(object_list) > 1:
+                raise MultipleObjectsReturned("More than '%s' matched '%s'." % (self._meta.object_class.__name__, stringified_kwargs))
+
+            return object_list[0]
+        except ValueError:
+            raise NotFound("Invalid resource lookup data provided (mismatched type).")
 
     def obj_create(self, bundle, request=None, **kwargs):
         bundle.obj = self._meta.object_class()
